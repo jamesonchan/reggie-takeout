2619c250bacac9846f9165397d018890106e162a
Jameson Chen <[jamesonychan@gmail.com]>
2023-01-07 23:19:07 -0500

    * Added MybatisPlusConfig * Added Employee pagination controller

--- src/main/java/com/project/reggie/controller/EmployeeController.java
+++ src/main/java/com/project/reggie/controller/EmployeeController.java
@@ -1,16 +1,15 @@
 package com.project.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.project.reggie.common.R;
 import com.project.reggie.domain.Employee;
 import com.project.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
@@ -85,6 +84,26 @@ public class EmployeeController {
 
         employeeService.save(employee);
         return R.success("save new employee successfully");
+    }
+
+    /**
+     * Employee info pagination
+     *
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        log.info("page = {}, pageSize = {}, name = {}", page, pageSize, name);
+        Page pageInfo = new Page(page, pageSize);
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+
+        queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        employeeService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
     }
 
 
