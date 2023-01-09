2f2505c1c4ee411aa9bda1aa4ac844dac2c287e3
Jameson Chen <[jamesonychan@gmail.com]>
2023-01-08 21:17:31 -0500

    * Added page in CategoryController * Added @Service in CategoryServiceImpl to fix the bug failed to find bean

--- src/main/java/com/project/reggie/controller/CategoryController.java
+++ src/main/java/com/project/reggie/controller/CategoryController.java
@@ -1,14 +1,13 @@
 package com.project.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.project.reggie.common.R;
 import com.project.reggie.domain.Category;
 import com.project.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/category")
@@ -19,6 +18,7 @@ public class CategoryController {
 
     /**
      * save new category
+     *
      * @param category
      * @return
      */
@@ -27,5 +27,14 @@ public class CategoryController {
         log.info("save new category: {}", category);
         categoryService.save(category);
         return R.success("new category saved successfully");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize) {
+        Page<Category> pageInfo = new Page<>(page, pageSize);
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Category::getSort);
+        categoryService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
     }
 }
--- src/main/java/com/project/reggie/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/project/reggie/service/impl/CategoryServiceImpl.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.project.reggie.domain.Category;
 import com.project.reggie.mapper.CategoryMapper;
 import com.project.reggie.service.CategoryService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
 }
